9a73e602f2338583c082380e49a63d683683fd06
Cameron Bytheway <cameron@theflokk.com>
2013-11-21 13:34:10 -0700

    add assign method

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
  * Module dependencies
  */
 
+var parse = require('urlparser').parse;
 var qs = require('querystring');
+var jsonp = require('jsonp');
 
 /**
  * keep a config
@@ -16,17 +18,30 @@ var config = {};
  * @param {String} event
  */
 
-exports = module.exports = function(event) {
-  if (!config.app || !config.user) throw new Error('pivot not initialized. use `pivot.init(\'app_id\', \'user_id\')`');
+exports = module.exports = function(event, conf) {
+  conf = conf || config;
+  if (!conf.app || !conf.user) throw new Error('pivot not initialized. use `pivot.init(\'app_id\', \'user_id\')`');
 
   var data = {
-    a: config.app,
+    a: conf.app,
     e: event,
-    u: config.user
+    u: conf.user
   };
 
-  track(config.track, data, config);
+  if (conf.test) data.t = 1;
+
+  if (typeof event === 'string') return track(conf.events, data);
+
+  delete data.e;
+  assign(conf.assignments, data, event);
 };
+
+/**
+ * Alias the main function
+ */
+
+exports.assign = exports;
+exports.event = exports;
 
 /**
  * Set a config option
@@ -48,18 +63,46 @@ exports.set = function(key, value) {
  */
 
 exports.init = function(app, user, opts) {
+  config = defaults(app, user, opts);
+  return exports;
+};
+
+/**
+ * Create an independent client
+ */
+
+exports.client = function(app, user, opts) {
+  var conf = defaults(app, user, opts);
+
+  return function client(event) {
+    return exports(event, conf);
+  };
+};
+
+function defaults(url, user, opts) {
   opts = opts || {};
+  var conf = {};
 
-  config.app = app;
-  config.user = user;
-  config.host = opts.host || 'https://api.pivotapp.io';
-  config.track = opts.track || config.host + '/track';
-  config.debug = opts.debug || false;
-  config.test = opts.test || false;
-};
+  url = parse(url);
 
+  conf.url = url;
+  conf.app = url.host.username;
 
-function track(url, data, opts) {
-  if (opts.test) data['t'] = 1;
+  url.host.username = null;
+
+  conf.user = user;
+  conf.events = url.toString() + '/events';
+  conf.assignments = url.toString() + '/assignments';
+  conf.debug = opts.debug || false;
+  conf.test = opts.test || false;
+  return conf;
+}
+
+function track(url, data) {
   (new Image).src = url + '?' + qs.stringify(data);
-};
+}
+
+function assign(url, data, fn) {
+  url = url + '?' + qs.stringify(data)
+  jsonp(url, fn);
+}
